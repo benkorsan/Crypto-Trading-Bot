10edd7eba983e744d571e542c4a12ff97b301538
benkorsan <103997850+benkorsan@users.noreply.github.com>
2024-12-17 18:05:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_KEY = "xxxxxxxxxx"
+API_SECRET = "xxxxxxxxxxxx"
+BASE_URL = "https://api.mexc.com"
+
+
+# Risk Yönetimi
+STOP_LOSS_PERCENT = 0.05  # %5 Stop Loss
+TRAILING_STOP_PERCENT = 0.03  # %3 Kar Takibi (Trailing Stop)
+TRADE_PERCENTAGE = 0.10  # Bakiyenin %20'si ile işlem açılacak
