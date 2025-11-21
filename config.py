a7d4155dd9323ada105a7440763a3e791ca0f990
benkorsan <103997850+benkorsan@users.noreply.github.com>
2025-11-21 13:08:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-API_KEY = "xxxxxxxxxx"
-API_SECRET = "xxxxxxxxxxxx"
-BASE_URL = "https://api.mexc.com"
+BINANCE_API_KEY = "xxxxxxxxxx"
+BINANCE_SECRET_KEY = "xxxxxxxxxxxx"
 
 
-# Risk Yönetimi
-STOP_LOSS_PERCENT = 0.05  # %5 Stop Loss
-TRAILING_STOP_PERCENT = 0.03  # %3 Kar Takibi (Trailing Stop)
-TRADE_PERCENTAGE = 0.10  # Bakiyenin %20'si ile işlem açılacak
+# Risk Yönetimi kaldırıldı 
